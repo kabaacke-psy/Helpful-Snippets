94f16cf52d0bd962a0301ff5dd68d0c560d70ee4
Kyle Baacke <55969421+kabaacke-psy@users.noreply.github.com>
2022-01-31 10:09:15 -0600

    cft moved to reflect state

---
+++ Snippets/cluster_file_transfer.py
@@ -0,0 +1,27 @@
+# You may want to transfer a file or list of files to or from the cluster without using a physical drive. This can be accomplished over SSH using SCP. Alternatively, you can run SCP via python in order to streamline your analysis or data management processes.
+
+# You will need the following python packages
+
+import os
+import shutil
+import datetime as dt
+import paramiko
+from scp import SCPClient
+import zipfile # Only required if you want to zip before sending
+import getpass # Used for getting password from the user in a prompt rather than having your password in the code (which is a very, very bad practice)
+
+# The following line is ot enable the script to work on all opreating systems by dynamically assigning what the file deliminer will be.
+sep = os.path.sep
+
+# This line is to specify what the deliminer is on the remote system. Sinc ethe cluster is linux, the following should always work
+dest_sep = '/'
+
+# The following function will create a cirtual SSH session to be used ot call the SCP commands.
+def createSSHClient(server, port, user, password):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(server, port, user, password)
+    return client
+
+
