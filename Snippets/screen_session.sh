3adc07289216a7d04f5b46248a3178fbcfd2f826
kabaacke-psy <55969421+kabaacke-psy@users.noreply.github.com>
2022-02-09 09:19:07 -0600

    Update screen_session.sh

--- Snippets/screen_session.sh
+++ Snippets/screen_session.sh
@@ -1,4 +1,13 @@
-# https://linuxize.com/post/how-to-use-linux-screen/
+# @Authors: 
+#   Kyle Baacke; kbaacke2@illinois.edu; https://github.com/kabaacke-psy
+# @DateCreated: 02/07/2022
+# @DateUpdated: 02/07/2022
+# Description:
+#   {DESCRIPTION}
+# Notes and Qualifiers:
+#   {NOTES}
+# Helpful Links:
+#   https://linuxize.com/post/how-to-use-linux-screen/
 
 # The screen command allows you to save your progress in a  remotete session when accessing the cluster via SSH
 
