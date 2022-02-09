b74c062be9192d8222003238f067d7b84912916b
kabaacke-psy <55969421+kabaacke-psy@users.noreply.github.com>
2022-02-09 09:19:11 -0600

    Update SGE_via_python.py

--- Snippets/SGE_via_python.py
+++ Snippets/SGE_via_python.py
@@ -1,4 +1,12 @@
-https://pyabc.readthedocs.io/en/tidyup_rpy2/api_sge.html
-
-https://pyabc.readthedocs.io/en/tidyup_rpy2/sge.html
+# @Authors: 
+#   Kyle Baacke; kbaacke2@illinois.edu; https://github.com/kabaacke-psy
+# @DateCreated: 02/07/2022
+# @DateUpdated: 02/07/2022
+# Description:
+#   {DESCRIPTION}
+# Notes and Qualifiers:
+#   {NOTES}
+# Helpful Links:
+#   https://pyabc.readthedocs.io/en/tidyup_rpy2/api_sge.html
+#   https://pyabc.readthedocs.io/en/tidyup_rpy2/sge.html
 
