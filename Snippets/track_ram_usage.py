bca4653df247ebc923791ed913553a995017fdf7
kabaacke-psy <55969421+kabaacke-psy@users.noreply.github.com>
2022-02-09 09:19:23 -0600

    Update track_ram_usage.py

--- Snippets/track_ram_usage.py
+++ Snippets/track_ram_usage.py
@@ -1,3 +1,12 @@
-#https://medium.com/survata-engineering-blog/monitoring-memory-usage-of-a-running-python-program-49f027e3d1ba
-#https://docs.python.org/3/library/tracemalloc.html#record-the-current-and-peak-size-of-all-traced-memory-blocks
+# @Authors: 
+#   Kyle Baacke; kbaacke2@illinois.edu; https://github.com/kabaacke-psy
+# @DateCreated: 02/07/2022
+# @DateUpdated: 02/07/2022
+# Description:
+#   {DESCRIPTION}
+# Notes and Qualifiers:
+#   {NOTES}
+# Helpful Links:
+#   https://medium.com/survata-engineering-blog/monitoring-memory-usage-of-a-running-python-program-49f027e3d1ba
+#   https://docs.python.org/3/library/tracemalloc.html#record-the-current-and-peak-size-of-all-traced-memory-blocks
 
