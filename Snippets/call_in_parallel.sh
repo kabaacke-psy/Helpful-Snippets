6516127ddfd4c3a6fda40bf33e5217c839dae0a3
kabaacke-psy <55969421+kabaacke-psy@users.noreply.github.com>
2022-02-14 13:59:25 -0600

    Added better description

--- Snippets/call_in_parallel.sh
+++ Snippets/call_in_parallel.sh
@@ -3,31 +3,40 @@
 # @DateCreated: 02/07/2022
 # @DateUpdated: 02/07/2022
 # Description:
-#   {DESCRIPTION}
+#   You may find yourself wanting to run a process in parallel (i.e. multiple processes running simultaneously) via the native shell (or zshell) native to your computational environment. This snippet provides an example of how to do so without the use of any non-generic packages or functions. This can enable you to make full use of the compute resources available without the additional overhead of installing other tools. The process can be divided into 3 main steps: 1) establishing the resource pool available to use, 2) defining a function to run in parallel if there are multipe components of a process that need to be run for each iteration, and 3) calling the function in parallel
 # Notes and Qualifiers:
-#   {NOTES}
+#   This code has been written and tested in BASH on native linux installations (OpenSUSE) and on Ubuntu for Windows (20.04.2 using WSL 2). 
 
-# Running terminal commans in parallel is a great way to take advantage of the compute resources available to you.
+# 1) Identifying your resource pool
+
 # Before you run anyting in parallel, you should first establish how many CPUs ara avialble for use.
 # The following command will give you details about the CPUs on the current machine
 lscpu
 
-
 # Always make sure you will not cause problems for other users by checking to see if anyone else is currenlty using the same machine.
-# You can do this and learn more about the currenlt memory availability using the following command.
+# You can do this and learn more about the current memory and thread availability using either of the following commands. 
 
 top
+# or
+htop
 
 # This will give you an active diplay of the processes currently running on the machine, including CPU and memory usage and runtime.
 # Use q to exit the top display.
 
-q
+# 2) Defining your function
 
-# Now that you know what you have to work with, you need to set up a function to run in paralell
+# Now that you know what you have to work with, you need to set up a function to run in parallel
 # The following function takes three arguements (subject, time, and run, in that order).
 # It then uses these arguements to call a python function (nifty2numpy_parcellation.py).
+# First, lets start with some global constant variables
+
 OUTDIR2=/mnt/usb1/HCP_1200_ICA-AROMA
 TIME=tfMRI_MOTOR
+# We need a list of subjects to iterate through. The following command creates a list out of each folder in the directory we assigned to OUTDIR2
+SUBLIST=$(ls $OUTDIR2)
+# Now assign the number of threads to use as a variable
+N=10
+
 parcellate(){
   # Use $ fllowed by a number to take an arguement passed to the function and assign it to a variable
   local subject=$1
@@ -41,10 +50,8 @@ parcellate(){
     --metadata /mnt/usb1/Code/MSC_HCP/
 }
 
-# We need a list of subjects to iterate through. The following command creates a list out of each folder in the directory we assigned to OUTDIR2
-SUBLIST=$(ls $OUTDIR2)
-# Now assign the number of threads to use as a variable
-N=10
+# 3) Calling in parallel
+
 # We can iterate through the subjects in $SUBLIST and the runs within each subject using nested for loops
 for SUBJECT in $SUBLIST; do
   for RUN in RL LR; do
@@ -58,3 +65,4 @@ for SUBJECT in $SUBLIST; do
    done
 done
 
+# Note that you can keep 'htop' running in a second window to track your use of resources in real time. This is a good way to keep track of how far along you are, as well as what bottlenecks you have when running N threads at a time. For instance, you may find that you can only run 4 at a time, even through you have 12 threads because of a memory constraint.
